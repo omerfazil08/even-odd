7ab9891e2aebbba0a4f8112e27c78129700c87d9
omerfazil08 <omerfazilorhan@gmail.com>
2024-12-27 20:32:52 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	double x, y, z;
+
+	cout << "Give me Number: ";
+		cin >> x;
+
+	if((pow(-1, x) == 1))
+	{
+		cout << "This number is an neven number.";
+
+}
+
+
+	else if((pow(-1, x) == -1))
+	{
+		cout << "This number is an odd number.";
+
+
+
+	}
+
+
+	return 0;
+}
